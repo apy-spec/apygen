5a42dbe86683fda7677c2fdd493034f333a633ff
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-04-23 22:45:27 +0200

    Extract analyse_workdir

--- crates/apygen/src/main.rs
+++ crates/apygen/src/main.rs
@@ -1,51 +1,22 @@
-use apy;
-use apygen_python_analysis::abstract_environment::Identifier;
-use apygen_python_analysis::converter::v1::convert_apy_v1;
-use apygen_python_analysis::finder::filesystem::{AbsolutePathBuf, LocalFilesystem};
-use apygen_python_analysis::finder::pathfinder::PathFinder;
-use apygen_python_analysis::worklist::cfg_worklist;
-use rayon::prelude::*;
-use std::collections::{HashMap, HashSet};
+use apy::Apy;
+use apygen_python_analysis::{AbsolutePathBuf, LocalFilesystem, analyse_workdir};
 use std::fs::File;
-use std::path::PathBuf;
-use std::sync::Arc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let working_dir = AbsolutePathBuf::current_dir()?;
     let python_paths = vec![];
-    let working_dir = AbsolutePathBuf::try_from(PathBuf::from("."))?;
+    let stubs_paths = vec![];
+    let typeshed = Some(working_dir.join("vendor/typeshed/stdlib"));
 
-    let finder = PathFinder::new(
-        Arc::new(LocalFilesystem),
+    let apy: Apy = analyse_workdir(
+        LocalFilesystem,
         python_paths,
-        Vec::new(),
-        Some(working_dir),
-        None,
+        stubs_paths,
+        working_dir,
+        typeshed,
     );
 
-    let specs: HashMap<Identifier, _> = finder.get_specs();
-
-    let target_modules: HashSet<_> = specs
-        .par_iter()
-        .filter_map(|(identifier, finder_spec)| {
-            if finder
-                .working_directory()
-                .is_some_and(|working_directory| finder_spec.spec.is_inside(working_directory))
-            {
-                Some(identifier.clone())
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let (namespaces, cfgs) = cfg_worklist(specs, target_modules).unwrap();
-
-    let apy_v1_spec = convert_apy_v1(&namespaces, &cfgs);
-
-    let apy_spec = apy::Apy::V1(apy_v1_spec);
-
-    apy_spec.to_json_writer(File::create("apy.json")?)?;
-    apy_spec.to_yaml_writer(&mut File::create("apy.yaml")?)?;
+    apy.to_yaml_writer(&mut File::create("apy.yaml")?)?;
 
     Ok(())
 }
--- crates/apygen_python_analysis/src/lib.rs
+++ crates/apygen_python_analysis/src/lib.rs
@@ -1,7 +1,55 @@
-pub use apygen_finder as finder;
+use crate::converter::v1::convert_apy_v1;
+use crate::worklist::cfg_worklist;
+pub use apy;
+use apy::v1::Identifier;
 pub use apygen_analysis as analysis;
+pub use apygen_finder as finder;
+pub use finder::filesystem::{AbsolutePathBuf, Filesystem, LocalFilesystem};
+pub use finder::pathfinder::PathFinder;
+use log::debug;
+use rayon::prelude::*;
+use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
 
-pub mod genkill;
 pub mod abstract_environment;
-pub mod worklist;
 pub mod converter;
+pub mod genkill;
+pub mod worklist;
+
+pub fn analyse_workdir(
+    filesystem: impl Filesystem,
+    python_paths: Vec<AbsolutePathBuf>,
+    stubs_paths: Vec<AbsolutePathBuf>,
+    working_directory: AbsolutePathBuf,
+    typeshed_path: Option<AbsolutePathBuf>,
+) -> apy::Apy {
+    let finder = PathFinder::new(
+        Arc::new(filesystem),
+        python_paths,
+        stubs_paths,
+        Some(working_directory),
+        typeshed_path,
+    );
+
+    let specs: HashMap<Identifier, _> = finder.get_specs();
+
+    let target_modules: HashSet<_> = specs
+        .par_iter()
+        .filter_map(|(identifier, finder_spec)| {
+            if finder_spec.spec.is_inside(finder.working_directory()?) {
+                Some(identifier.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let (namespaces, cfgs) = cfg_worklist(specs, target_modules).unwrap();
+
+    debug!("Modules: {}", cfgs.len());
+    debug!("Locations: {}", namespaces.locations.len());
+
+    let apy_v1 = convert_apy_v1(&namespaces, &cfgs);
+
+    apy::Apy::V1(apy_v1)
+}
