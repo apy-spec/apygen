85c75b36ca8e02b8312c95d10db12c8ac2381746
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-07-13 01:53:20 +0200

    Refactor AbstractState

--- crates/apygen_analysis/src/abstract_state.rs
+++ crates/apygen_analysis/src/abstract_state.rs
@@ -1,17 +1,22 @@
-pub trait AbstractState<'a> {
+pub trait AbstractState {
     type Key;
     type AbstractValue;
 
-    fn get(&self, key: &Self::Key) -> Option<&'a Self::AbstractValue>;
-    fn get_mut(&mut self, key: &Self::Key) -> Option<&'a mut Self::AbstractValue>;
+    fn get(&self, key: &Self::Key) -> Option<&Self::AbstractValue>;
+    fn get_mut(&mut self, key: &Self::Key) -> Option<&mut Self::AbstractValue>;
+    fn get_or_insert(
+        &mut self,
+        key: Self::Key,
+        abstract_value: Self::AbstractValue,
+    ) -> &mut Self::AbstractValue;
     fn insert(
         &mut self,
         key: Self::Key,
         abstract_value: Self::AbstractValue,
-    ) -> Option<&'a mut Self::AbstractValue>;
+    ) -> &mut Self::AbstractValue;
 }
 
-pub struct AbstractStateProxy<'a, S: AbstractState<'a>, P: AbstractState<'a>> {
+pub struct AbstractStateProxy<'a, S: AbstractState, P: AbstractState> {
     pub abstract_state: &'a S,
     pub proxy: P,
 }
@@ -20,27 +25,46 @@ impl<
     'a,
     K: Clone,
     A: Clone,
-    S: AbstractState<'a, Key = K, AbstractValue = A>,
-    P: AbstractState<'a, Key = K, AbstractValue = A>,
-> AbstractState<'a> for AbstractStateProxy<'a, S, P>
+    S: AbstractState<Key = K, AbstractValue = A>,
+    P: AbstractState<Key = K, AbstractValue = A>,
+> AbstractState for AbstractStateProxy<'a, S, P>
 {
     type Key = K;
     type AbstractValue = A;
 
-    fn get(&self, key: &Self::Key) -> Option<&'a Self::AbstractValue> {
+    fn get(&self, key: &Self::Key) -> Option<&Self::AbstractValue> {
         self.proxy.get(key).or_else(|| self.abstract_state.get(key))
     }
 
-    fn get_mut(&mut self, key: &K) -> Option<&'a mut A> {
-        if let Some(value) = self.proxy.get_mut(key) {
-            Some(value)
+    fn get_mut(&mut self, key: &K) -> Option<&mut A> {
+        if let Some(abstract_value) = self.abstract_state.get(key) {
+            Some(
+                self.proxy
+                    .get_or_insert(key.clone(), abstract_value.clone()),
+            )
         } else {
-            self.proxy
-                .insert(key.clone(), self.abstract_state.get(key)?.clone())
+            self.proxy.get_mut(key)
         }
     }
 
-    fn insert(&mut self, key: K, value: A) -> Option<&'a mut A> {
-        self.proxy.insert(key, value)
+    fn get_or_insert(
+        &mut self,
+        key: Self::Key,
+        abstract_value: Self::AbstractValue,
+    ) -> &mut Self::AbstractValue {
+        let new_abstract_value = self
+            .abstract_state
+            .get(&key)
+            .cloned()
+            .unwrap_or(abstract_value);
+        self.proxy.get_or_insert(key, new_abstract_value)
+    }
+
+    fn insert(
+        &mut self,
+        key: Self::Key,
+        abstract_value: Self::AbstractValue,
+    ) -> &mut Self::AbstractValue {
+        self.proxy.insert(key, abstract_value)
     }
 }
