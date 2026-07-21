4b6878a7c42d9e584fb5059cfa84163a10f182f9
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-07-21 19:12:35 +0200

    Improve par_analysis performance

--- crates/apygen_analysis/src/rayon.rs
+++ crates/apygen_analysis/src/rayon.rs
@@ -5,7 +5,7 @@ use std::collections::{BTreeMap, BTreeSet};
 
 pub fn par_analysis<
     N: Clone + Ord + Send,
-    S: Eq + Send,
+    S: Eq + Clone + Send,
     A: Sync,
     E: Send,
     T: Send + Sync + GraphAnalyser<Node = N, AbstractState = S, AnalysisState = A, Error = E>,
@@ -30,23 +30,36 @@ pub fn par_analysis<
         let new_states = worklist
             .into_par_iter()
             .map(|node| {
-                let mut new_abstract_states = BTreeMap::new();
-
                 let abstract_state = analyser.analyse_node(&analysis_state, &node)?;
 
-                for next_node in analyser.next_nodes(&node)? {
-                    let Some(updated_abstract_state) = analyser.update_abstract_state(
-                        &analysis_state,
-                        &node,
-                        &next_node,
-                        &abstract_state,
-                    )?
-                    else {
-                        continue;
-                    };
-
-                    let (should_update, new_abstract_state) =
-                        match analyser.get_abstract_state(&analysis_state, next_node)? {
+                Ok(analyser
+                    .next_nodes(&node)?
+                    .map(|next_node| (node.clone(), next_node.clone(), abstract_state.clone()))
+                    .collect::<Vec<_>>())
+            })
+            .map(|analysed_nodes| {
+                analysed_nodes?
+                    .into_par_iter()
+                    .map(|(node, next_node, abstract_state)| {
+                        if let Some(updated_abstract_state) = analyser.update_abstract_state(
+                            &analysis_state,
+                            &node,
+                            &next_node,
+                            &abstract_state,
+                        )? {
+                            Ok(Some((next_node, updated_abstract_state)))
+                        } else {
+                            Ok(None)
+                        }
+                    })
+                    .filter_map(|next_abstract_state| next_abstract_state.transpose())
+                    .collect::<Result<Vec<_>, _>>()
+            })
+            .map(|updated_nodes| {
+                updated_nodes?
+                    .into_par_iter()
+                    .map(|(next_node, updated_abstract_state)| {
+                        match analyser.get_abstract_state(&analysis_state, &next_node)? {
                             Some(next_node_abstract_state) => {
                                 let new_abstract_state = analyser.merge(
                                     &analysis_state,
@@ -54,20 +67,17 @@ pub fn par_analysis<
                                     &next_node_abstract_state,
                                     &updated_abstract_state,
                                 )?;
-                                (
-                                    new_abstract_state != *next_node_abstract_state,
-                                    new_abstract_state,
-                                )
+                                if new_abstract_state != updated_abstract_state {
+                                    Ok(Some((next_node, new_abstract_state)))
+                                } else {
+                                    Ok(None)
+                                }
                             }
-                            None => (true, updated_abstract_state),
-                        };
-
-                    if should_update {
-                        new_abstract_states.insert(next_node.clone(), new_abstract_state);
-                    }
-                }
-
-                Ok(new_abstract_states)
+                            None => Ok(Some((next_node, updated_abstract_state))),
+                        }
+                    })
+                    .filter_map(|new_abstract_state| new_abstract_state.transpose())
+                    .collect::<Result<BTreeMap<_, _>, _>>()
             })
             .try_reduce(
                 || BTreeMap::new(),
@@ -78,11 +88,11 @@ pub fn par_analysis<
                                 entry.insert(new_state);
                             }
                             Entry::Occupied(entry) => {
-                                let entry = entry.into_mut();
-                                *entry = analyser.merge(
+                                let current = entry.into_mut();
+                                *current = analyser.merge(
                                     &analysis_state,
                                     &next_node,
-                                    entry,
+                                    current,
                                     &new_state,
                                 )?
                             }
