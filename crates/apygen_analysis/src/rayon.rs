0c28fe7be3b803609f66bb1038b95d142b83d0f6
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-07-21 19:49:49 +0200

    Improve performance of par_analysis

--- crates/apygen_analysis/src/rayon.rs
+++ crates/apygen_analysis/src/rayon.rs
@@ -3,6 +3,25 @@ use rayon::prelude::*;
 use std::collections::btree_map::Entry;
 use std::collections::{BTreeMap, BTreeSet};
 
+fn update_merge<N: Clone + Ord, S, A, E>(
+    analyser: &impl GraphAnalyser<Node = N, AbstractState = S, AnalysisState = A, Error = E>,
+    analysis_state: &A,
+    abstract_states: &mut BTreeMap<N, S>,
+    node: N,
+    state: S,
+) -> Result<(), E> {
+    match abstract_states.entry(node.clone()) {
+        Entry::Vacant(entry) => {
+            entry.insert(state);
+        }
+        Entry::Occupied(entry) => {
+            let current = entry.into_mut();
+            *current = analyser.merge(&analysis_state, &node, current, &state)?
+        }
+    }
+    Ok(())
+}
+
 pub fn par_analysis<
     N: Clone + Ord + Send,
     S: Eq + Clone + Send,
@@ -37,66 +56,58 @@ pub fn par_analysis<
                     .map(|next_node| (node.clone(), next_node.clone(), abstract_state.clone()))
                     .collect::<Vec<_>>())
             })
-            .map(|analysed_nodes| {
-                analysed_nodes?
-                    .into_par_iter()
-                    .map(|(node, next_node, abstract_state)| {
-                        if let Some(updated_abstract_state) = analyser.update_abstract_state(
-                            &analysis_state,
-                            &node,
-                            &next_node,
-                            &abstract_state,
-                        )? {
-                            Ok(Some((next_node, updated_abstract_state)))
-                        } else {
-                            Ok(None)
-                        }
-                    })
-                    .filter_map(|next_abstract_state| next_abstract_state.transpose())
-                    .collect::<Result<Vec<_>, _>>()
-            })
-            .map(|updated_nodes| {
-                updated_nodes?
-                    .into_par_iter()
-                    .map(|(next_node, updated_abstract_state)| {
-                        match analyser.get_abstract_state(&analysis_state, &next_node)? {
-                            Some(next_node_abstract_state) => {
-                                let new_abstract_state = analyser.merge(
-                                    &analysis_state,
-                                    &next_node,
-                                    &next_node_abstract_state,
-                                    &updated_abstract_state,
-                                )?;
-                                if new_abstract_state != updated_abstract_state {
-                                    Ok(Some((next_node, new_abstract_state)))
-                                } else {
-                                    Ok(None)
-                                }
+            .try_reduce(
+                || Vec::new(),
+                |mut acc, analysed_nodes| {
+                    acc.extend(analysed_nodes);
+                    Ok(acc)
+                },
+            )?
+            .into_par_iter()
+            .map(|(node, next_node, abstract_state)| {
+                let Some(updated_abstract_state) = analyser.update_abstract_state(
+                    &analysis_state,
+                    &node,
+                    &next_node,
+                    &abstract_state,
+                )?
+                else {
+                    return Ok(None);
+                };
+
+                Ok(
+                    match analyser.get_abstract_state(&analysis_state, &next_node)? {
+                        Some(next_node_abstract_state) => {
+                            let new_abstract_state = analyser.merge(
+                                &analysis_state,
+                                &next_node,
+                                &next_node_abstract_state,
+                                &updated_abstract_state,
+                            )?;
+                            if new_abstract_state != updated_abstract_state {
+                                Some((next_node, new_abstract_state))
+                            } else {
+                                None
                             }
-                            None => Ok(Some((next_node, updated_abstract_state))),
                         }
-                    })
-                    .filter_map(|new_abstract_state| new_abstract_state.transpose())
-                    .collect::<Result<BTreeMap<_, _>, _>>()
+                        None => Some((next_node, updated_abstract_state)),
+                    },
+                )
             })
+            .try_fold(
+                || BTreeMap::new(),
+                |mut acc, new_abstract_state_option| {
+                    if let Some((next_node, new_state)) = new_abstract_state_option? {
+                        update_merge(analyser, &analysis_state, &mut acc, next_node, new_state)?;
+                    }
+                    Ok(acc)
+                },
+            )
             .try_reduce(
                 || BTreeMap::new(),
                 |mut acc, new_states| {
                     for (next_node, new_state) in new_states {
-                        match acc.entry(next_node.clone()) {
-                            Entry::Vacant(entry) => {
-                                entry.insert(new_state);
-                            }
-                            Entry::Occupied(entry) => {
-                                let current = entry.into_mut();
-                                *current = analyser.merge(
-                                    &analysis_state,
-                                    &next_node,
-                                    current,
-                                    &new_state,
-                                )?
-                            }
-                        }
+                        update_merge(analyser, &analysis_state, &mut acc, next_node, new_state)?;
                     }
                     Ok(acc)
                 },
