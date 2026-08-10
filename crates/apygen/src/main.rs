38fb5b1ba764f92b18ce9537442cfb9baae47778
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-08-10 13:26:10 +0200

    Fix main

--- crates/apygen/src/main.rs
+++ crates/apygen/src/main.rs
@@ -1,15 +1,30 @@
 use apy::Apy;
 use apygen::analyse_workdir;
-use apygen_finder::filesystem::{AbsolutePathBuf, LocalFilesystem};
+use apygen::finder::filesystem::{AbsolutePathBuf, LocalFilesystem};
 use std::fs::File;
+use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     let working_dir = AbsolutePathBuf::current_dir()?;
-    let python_paths = vec![];
+
+    let output = Command::new("python")
+        .arg("-c")
+        .arg("import sys; print(sys.path)")
+        .output()?; // TODO: improve
+
+    let python_paths = String::from_utf8(output.stdout)?
+        .trim()
+        .trim_start_matches('[')
+        .trim_end_matches(']')
+        .split(',')
+        .map(|value| value.trim().trim_matches('\'').trim_matches('"'))
+        .map(|path| working_dir.join(path))
+        .collect::<Vec<_>>();
+
     let stubs_paths = vec![];
-    let typeshed = Some(working_dir.join("vendor/typeshed/stdlib"));
+    let typeshed = Some(AbsolutePathBuf::current_dir()?.join("vendor/typeshed/stdlib"));
 
     let apy: Apy = analyse_workdir(
         LocalFilesystem,
